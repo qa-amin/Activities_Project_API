2488deba5a3e9126c96303c7cfcb1c1f8adbe383
Mohammad Amin Ghaderi <m.a.ghaderi78@gmail.com>
2023-11-28 00:09:44 +0330

    add ActivitiesManagement.Domain and implement Activity Entity

---
+++ ActivitiesManagement.Domain/Entities/ActivityAgg/Activity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActivitiesManagement.Domain.Entities.ActivityAgg
+{
+    public class Activity
+    {
+        public Guid Id { get; private set; }
+        public string  Title { get; private set; }
+        public DateTime Date { get; private set; }
+        public string Description { get; private set; }
+        public string Category { get; private set; }
+        public string City { get; private set; }
+        public string Venue { get; private set; }
+
+    }
+}
