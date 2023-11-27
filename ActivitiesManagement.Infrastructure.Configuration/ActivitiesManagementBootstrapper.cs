ccde2c1d4d44f2f556060363117e447fd441d972
Mohammad Amin Ghaderi <m.a.ghaderi78@gmail.com>
2023-11-28 01:22:34 +0330

    add ActivitiesManagement.Infrastructure.EFCore && ActivitiesManagement.Infrastructure.Configuration

---
+++ ActivitiesManagement.Infrastructure.Configuration/ActivitiesManagementBootstrapper.cs
@@ -0,0 +1,14 @@
+﻿using ActivitiesManagement.Infrastructure.EFCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ActivitiesManagement.Infrastructure.Configuration
+{
+    public class ActivitiesManagementBootstrapper
+    {
+        public static void Config(IServiceCollection services, string cs)
+        {
+            services.AddDbContext<ActivitiesContext>(x => x.UseSqlServer(cs));
+        }
+    }
+}
---
+++ ActivitiesManagement.Infrastructure.EFCore/ActivitiesContext.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+
+namespace ActivitiesManagement.Infrastructure.EFCore
+{
+    public class ActivitiesContext : DbContext
+    {
+        public ActivitiesContext(DbContextOptions<ActivitiesContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Activity> Activities {get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            
+        }
+    }
+}
